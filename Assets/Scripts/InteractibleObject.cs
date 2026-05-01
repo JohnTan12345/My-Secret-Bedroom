81f7be50d7e6134598a1ebec3fa2964ea086fd4d
JohnTan12345 <s10259175@connect.np.edu.sg>
2026-05-02 03:31:25 +0800

    Added functions to interactibleObject

--- Assets/Scripts/InteractibleObject.cs
+++ Assets/Scripts/InteractibleObject.cs
@@ -1,3 +1,7 @@
+/*
+    Created by: John
+    Description: Handles the informative in-game text parts of the game as well as handling the tasks if any
+*/
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,12 +10,75 @@ public class InteractibleObject : MonoBehaviour
 {
     public string interactibleName;
     public List<GameText> textFlow;
-    public int currentTextElementNumber = 0;
+    private int currentTextElementNumber = 0;
     
+    private bool textStart = false;
     // Events
     public UnityEvent onTextsStart;
     public UnityEvent onTextChange;
-    public UnityEvent onTextsFinished;
+    public UnityEvent onTextsEnd;
+
+    // Initialization
+    void Awake()
+    {
+        
+    }
+
+    public GameText GetTextObject()
+    {
+        if (!textStart) {onTextsStart.Invoke(); currentTextElementNumber = 0; textStart = true;}
+        return textFlow[currentTextElementNumber];
+    }
+
+    public GameText NextTextObject(int option = 0)
+    {
+        GameText currentText = textFlow[currentTextElementNumber];
+        int nextTextElementNumber = currentTextElementNumber;
+        if (currentText.options.Count == 0)
+        {
+            if (currentText.jumpTo > -1)
+            {
+                nextTextElementNumber = currentText.jumpTo;
+            }
+            else
+            {
+                nextTextElementNumber++;
+
+                if (nextTextElementNumber == textFlow.Count)
+                {
+                    onTextsEnd.Invoke();
+                    textStart = false;
+                }
+            }
+        }
+        else if (0 > option && option > currentText.options.Count)
+        {
+            TextOptions chosenOption = currentText.options[option];
+            
+            if (chosenOption.jumpTo < 0) 
+            {
+                Debug.LogError($"Invalid text number to jump to\nText Element Number: {currentTextElementNumber}\nOption given: {option}\nOption to jump to: {currentText.options[option].jumpTo}");
+            }
+            else
+            {
+                nextTextElementNumber = chosenOption.jumpTo;
+            }
+        }
+        else
+        {
+            Debug.LogError($"Invalid option given\nText Element Number: {currentTextElementNumber}\nTotal option amount: {currentText.options.Count}\nOption given: {option}");
+        }
+
+        if (currentTextElementNumber != nextTextElementNumber)
+        {
+            onTextChange.Invoke();
+            currentTextElementNumber = nextTextElementNumber;
+        }
+
+        return textFlow[currentTextElementNumber];
+    }
+
+
 }
 
 [System.Serializable]
