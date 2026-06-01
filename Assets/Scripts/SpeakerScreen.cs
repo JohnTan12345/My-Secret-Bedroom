d81022dd7ecd64069ef2c145de36ccd85351eb5f
LucasTan123 <s10259047@connect.np.edu.sg>
2026-06-01 20:10:32 +0800

    Update SpeakerScreen.cs
    added script to change sprites after 5 s

--- Assets/Scripts/SpeakerScreen.cs
+++ Assets/Scripts/SpeakerScreen.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class SpeakerScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<Sprite> spriteList;     // Drag your sprites here
+    public Image screenImage;           // Drag the UI Image here
+    public float changeInterval = 2f;   // Seconds between changes
+
+    private int currentIndex = 0;
+
     void Start()
     {
-        
+        if (spriteList.Count > 0)
+        {
+            screenImage.sprite = spriteList[currentIndex];
+            InvokeRepeating("NextSprite", changeInterval, changeInterval);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void NextSprite()
     {
-        
+        currentIndex++;
+        if (currentIndex >= spriteList.Count)
+            currentIndex = 0; // loop back
+
+        screenImage.sprite = spriteList[currentIndex];
     }
 }
