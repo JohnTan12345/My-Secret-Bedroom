d58154ae781730e8ef0def57d567892aec028615
10267815E <s10267815@connect.np.edu.sg>
2026-07-10 11:42:38 +0800

    Imported SFX and wrote script to control SFX in corridor

---
+++ Assets/Scripts/Area Scripts/CorridorSFXController.cs
@@ -0,0 +1,38 @@
+/*
+    Created by: Xander
+    Description: Controls when corridor SFX are played and stopped
+*/
+using UnityEngine;
+
+public class CorridorSFXController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource corridorSFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            corridorSFX.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            corridorSFX.Stop();
+        }
+    }
+}
