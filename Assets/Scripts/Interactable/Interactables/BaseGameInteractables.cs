6342a1f2346a53e0cd4380d84013cae2cebb1a1a
JohnTan12345 <s10259175@connect.np.edu.sg>
2026-07-21 23:26:49 +0800

    Added variables, auto set interactableTask
    BaseGameInteractables:
    - Added variables
    - InteractableTask is now auto set based on current text
    - Commenting

--- Assets/Scripts/Interactable/Interactables/BaseGameInteractables.cs
+++ Assets/Scripts/Interactable/Interactables/BaseGameInteractables.cs
@@ -7,20 +7,30 @@ using UnityEngine;
 
 public abstract class BaseGameInteractables : MonoBehaviour
 {
-    protected EnterArea enterArea;
+    // Mandatory variables
+    [SerializeField]
     protected GameObject textUI;
+    [SerializeField]
     protected InteractableText interactableText;
-    protected virtual void Awake()
+
+    // Optional variables
+    [Header("Optional")]
+    [SerializeField]
+    [Tooltip("The interactable area")]
+    protected EnterArea enterArea;
+
+    // Internal variables
+    private protected InteractableTask interactableTask;
+
+    protected virtual void Awake() // Initialization
     {
         StartCoroutine(GameManagerEventListenerAdding());
-    }
 
-    private IEnumerator GameManagerEventListenerAdding()
-    {
-        yield return new WaitUntil(() => GameManager.instance != null);
+        // Event listeners adding
+        interactableTask = interactableText.GetGameText().taskToComplete;
 
-        GameManager.instance.onGameReset.AddListener(ResetInteractable);
         interactableText.onTextsEnd.AddListener(TextEnded);
+        interactableText.onTextChange.AddListener(SetInteractableTask);
 
         if (enterArea != null)
         {
@@ -29,10 +39,18 @@ public abstract class BaseGameInteractables : MonoBehaviour
         }
     }
 
-    protected virtual void OnDestroy()
+    private IEnumerator GameManagerEventListenerAdding() // Initialization for Game Manager events
+    {
+        yield return new WaitUntil(() => GameManager.instance != null);
+
+        GameManager.instance.onGameReset.AddListener(ResetInteractable);
+    }
+
+    protected virtual void OnDestroy() // Cleanup
     {
         GameManager.instance.onGameReset.RemoveListener(ResetInteractable);
         interactableText.onTextsEnd.RemoveListener(TextEnded);
+        interactableText.onTextChange.RemoveListener(SetInteractableTask);
 
         if (enterArea != null)
         {
@@ -41,29 +59,40 @@ public abstract class BaseGameInteractables : MonoBehaviour
         }
     }
 
+    // Resets the interactable to original position
     protected abstract void ResetInteractable();
+
+    // Disables the text UI after the texts ends
     protected virtual void TextEnded()
     {
         textUI.SetActive(false);
     }
 
+    // Enables the UI and highlights task objects when the player enters the interactable area
     protected virtual void OnPlayerEnterArea()
     {
         textUI.SetActive(true);
 
-        if (interactableText.GetGameText().taskToComplete != null)
+        if (interactableTask != null)
         {
-            interactableText.GetGameText().taskToComplete.HighlightObject(true);
+            interactableTask.HighlightObject(true);
         }
     }
 
+    // Disables the UI and unhighlights task objects when the player enters the interactable area
     protected virtual void OnPlayerExitArea()
     {
         textUI.SetActive(false);
 
-        if (interactableText.GetGameText().taskToComplete != null)
+        if (interactableTask != null)
         {
-            interactableText.GetGameText().taskToComplete.HighlightObject(false);
+            interactableTask.HighlightObject(false);
         }
+    }
+
+    // Sets the interactable task
+    private void SetInteractableTask()
+    {
+        interactableTask = interactableText.GetGameText().taskToComplete;
     }
 }
